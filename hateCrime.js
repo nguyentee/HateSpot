eb20d164033a5cdec7da0f05d9253e0b2a48986a
nguyentee <90297986+nguyentee@users.noreply.github.com>
2022-03-24 10:42:30 -0400

    Add files via upload

---
+++ hateCrime.js
@@ -0,0 +1,33 @@
+// set the dimensions and margins of the bar chart
+var visWidth = 500;
+var visHeight= 500;
+
+
+//add the map
+// Initialize and add the map
+
+/*the map was connected through the HTML page. There is no interactivity
+with the map and the bar chart yet
+
+The map was not successful through Google My Maps so we resorted to using Tableau*/
+
+//get the Data
+
+
+
+// bar chart code
+//script doesn't seem to work when placed in js file whereas it works if in html file
+/*
+var divElement = document.getElementById('viz1647801990160');
+var vizElement = divElement.getElementsByTagName('object')[0];
+vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
+*/
+
+
+
+
+//exit code goes here
+//svg.exit().remove();
